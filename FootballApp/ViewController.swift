b3d985ab1f05925ebc4509c187b38cac26ebf4a6
Thy Nguyen <nguyenthy2408@gmail.com>
2023-03-21 07:29:49 +0700

    Initial Commit

---
+++ FootballApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootballApp
+//
+//  Created by Thy Nguyen on 3/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
